909bbef66afad2b6e4f9930e303d75f5b157db4a
DarrylBrooks97 <darryl.brooks@aa.com>
2021-09-17 10:30:16 -0500

    chore: adding prettier and ts configs

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { Suspense } from 'react'
-import {ACESFilmicToneMapping} from 'three'
-import Earth from '../components/Earth'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Suspense } from 'react';
+import { ACESFilmicToneMapping } from 'three';
+import Earth from '../components/Earth';
 
 export default function IndexPage() {
-  return (
-    <>
-    <Canvas camera={{ position: [0, 0, 10],fov:40,far:10000 }} onCreated={({ gl }) => {
-    gl.gammaInput = true
-    gl.toneMapping = ACESFilmicToneMapping
-  }}>
-      <ambientLight intensity={2} />
-      <pointLight position={[40, 40, 40]} />
-      <Suspense fallback={null}>
-        <Earth position={[10, 0, 0]} gl={({gl})=>{return gl}}/>
-      </Suspense>
-      <OrbitControls enableZoom={false} maxPolarAngle={Math.PI } minPolarAngle={(Math.PI) / 2}/>
-    </Canvas>
-  </>
-  )
+	return (
+		<>
+			<Canvas
+				camera={{ position: [0, 0, 10], fov: 40, far: 10000 }}
+				onCreated={({ gl }) => {
+					gl.gammaInput = true;
+					gl.toneMapping = ACESFilmicToneMapping;
+				}}
+			>
+				<ambientLight intensity={2} />
+				<pointLight position={[40, 40, 40]} />
+				<Suspense fallback={null}>
+					<Earth
+						position={[10, 0, 0]}
+						gl={({ gl }) => {
+							return gl;
+						}}
+					/>
+				</Suspense>
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI}
+					minPolarAngle={Math.PI / 2}
+				/>
+			</Canvas>
+		</>
+	);
 }
