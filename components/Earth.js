8307ade57343a039861c02c140a1d566d06b1b4b
Darryl Brooks <darrylbrooks13@google.com>
2021-09-18 22:09:52 -0500

    chore: small updates, still needs a fix for panning around earth center

--- components/Earth.js
+++ components/Earth.js
@@ -1,23 +1,29 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useFrame, useLoader } from '@react-three/fiber';
 import { Sphere } from '@react-three/drei';
-import { TextureLoader, Box3, Object3D, Group, Scene as scene } from 'three';
+import { Box3, Group, Object3D, TextureLoader, Vector3 } from 'three';
 
 export default function Earth(props) {
-	const mesh = new Object3D();
-	const box = new Box3().setFromObject(mesh);
+	const mesh = useRef();
 	const colorMap = useLoader(TextureLoader, 'world.jpg');
+	// var box = new Box3().setFromObject(mesh);
+	var pivot = new Group();
 
-	// Update rotation axis
-	box.center(mesh.position);
-	mesh.position.multiplyScalar(-1);
+	// useFrame(() => {
+	// 	if (mesh.current) {
+	// 		box.getCenter(new Vector3(props.position));
+	// 		box.mesh.position.multiplyScalar(-1);
+	// 		pivot.add(mesh);
+	// 	}
+	// });
 
+	console.log(mesh.current);
 	// Animation
 	useFrame(() => (mesh.current.rotation.y += 0.001));
 
 	return (
-		<Sphere args={[2, 64, 64]} ref={mesh}>
-			<meshStandardMaterial attach="material" map={colorMap} />
+		<Sphere args={[2, 64, 64]} {...props} ref={mesh}>
+			<meshPhysicalMaterial attach="material" map={colorMap} />
 		</Sphere>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { Suspense } from 'react';
+import { Suspense, useRef } from 'react';
 import { ACESFilmicToneMapping } from 'three';
 import Earth from '../components/Earth';
 
@@ -8,20 +8,23 @@ export default function IndexPage() {
 	return (
 		<>
 			<Canvas
-				camera={{ position: [0, 0, 10], fov: 40, far: 10000 }}
+				camera={{ position: [2, 0, 10], fov: 40, far: 10000 }}
 				onCreated={({ gl }) => {
 					gl.gammaInput = true;
 					gl.toneMapping = ACESFilmicToneMapping;
 				}}
 			>
-				<ambientLight intensity={2} />
+				<ambientLight intensity={3} />
 				<Suspense fallback={null}>
-					<Earth position={[5, 0, 0]} />
+					<Earth position={[2, 0, 1]} />
 				</Suspense>
 				<OrbitControls
+					enableDamping={true}
+					dampingFactor={0.05}
 					enableZoom={false}
 					maxPolarAngle={Math.PI / 1.9}
 					minPolarAngle={Math.PI / 3}
+					screenSpacePanning={true}
 				/>
 			</Canvas>
 		</>
